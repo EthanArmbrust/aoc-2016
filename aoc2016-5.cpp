07ff63c0ffce8a52ca196a09dac16ea71cacef7e
Ethan Armbrust <EthanArmbrust@github.com>
2019-04-11 12:06:11 -0400

    day 5 done

---
+++ aoc2016-5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "hash-library/md5.h"
+
+using namespace std;
+
+int main(){
+
+	string input = "reyedfim";
+
+	MD5 md5;
+
+	string password;
+
+	int counter = 0;
+	while(password.length() < 8){
+		string hash = md5(input + to_string(counter));
+		if(hash.substr(0, 5) == "00000"){
+			password += hash[5];
+		}
+		counter++;
+	}
+
+	cout << password << endl;
+	
+
+	return 0;
+}
---
+++ aoc2016-5p2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include "hash-library/md5.h"
+
+using namespace std;
+
+int main(){
+
+	string input = "reyedfim";
+	MD5 md5;
+	string password = "--------";
+
+	int counter = 0;
+	while(password.find('-') != string::npos){
+		string hash = md5(input + to_string(counter));
+		if(hash.substr(0, 5) == "00000"){
+			if(hash[5] >= '0' && hash[5] <= '7' && password[hash[5] - '0'] == '-'){
+				password[hash[5] - '0'] = hash[6];
+				cout << password << endl;
+			}
+		}
+		counter++;
+	}
+
+	cout << password << endl;
+	
+
+	return 0;
+}
