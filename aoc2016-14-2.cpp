618e0495dc6454ca0d8f2d3ad6b729dfe9bcba0c
EthanArmbrust <ethanarm@buffalo.edu>
2019-07-31 23:16:23 -0400

    day 14 complete

--- aoc2016-14-2.cpp
+++ aoc2016-14-2.cpp
@@ -32,108 +32,49 @@ char find_repeat(string hash, int count = 3){
 
 int main(){
 
-//    string input = "zpqevtbw";
     string input = "zpqevtbw";
+    //string input = "abc";
     vector<string> hashes;
     MD5 md5;
-    int counter = 0;
-
-    string hash = md5(input + to_string(counter));
-    auto some = find_repeat(md5("abc39"));
+    int counter = 1;
 
     vector<key> potential_keys;
     vector<key> good_keys;
 
-    while(good_keys.size() < 64){
-        if(counter == 816){
-            //cout << "816" << endl;
-        }
+    while(!(good_keys.size() >= 64 && !potential_keys.empty())){
         string hash = md5(input + to_string(counter));
-        for(int i = 0; i < 2016; i++){
+        for(int j = 0; j < 2016; j++){
             hash = md5(hash);
         }
-        auto char_repeat = find_repeat(hash);
-        if(char_repeat != 0){
-            potential_keys.push_back({counter, char_repeat});
+        auto char_letter = find_repeat(hash);
+        if(char_letter != 0){
+            if(good_keys.size() < 64){
+                potential_keys.push_back({counter, char_letter});
+            }
             for(int i = 0; i < potential_keys.size(); i++){
                 key k = potential_keys[i];
                 if(counter - k.count_val > 1000){
                     potential_keys.erase(potential_keys.begin() + i);
+                    i--;
                 } 
                 else{
-                    if(verify_repeat(hash, 5, char_repeat) && k.count_val != counter){
+                    if(verify_repeat(hash, 5, k.letter) && k.count_val != counter){
                         good_keys.push_back(k);
-                        cout << good_keys.size() << endl;
                         potential_keys.erase(potential_keys.begin() + i);
-                        break;
-                    }    
+                        i--;
+                    } 
                 }
             }
         }
         counter++;
     }
 
-    std::sort(good_keys.begin(), good_keys.end(), compare_keys);
+    sort(good_keys.begin(), good_keys.end(), compare_keys);
     int count = 0;
     for(auto k : good_keys){
-        cout << count++ << ":" << k.count_val << endl;
+        cout << count << ": " << k.count_val << endl;
+        count++;
     }
     cout << good_keys[63].count_val << endl;
     return 0;
 }
-//*/
-
-/*
-
-int main(){
-
-    vector<string> hashes;
-    int counter = 0;
-    string input = "abc";
-
-    vector<int> keys;
-
-    MD5 md5;
-
-    while(keys.size() < 64){
-        string h;
-        if(hashes.size() <= counter){
-            string hash = md5(input + to_string(counter));
-            hashes.push_back(hash);
-            h = hash;
-        }
-        else{
-            h = hashes[counter];
-        }
-        auto l = find_repeat(h);
-        if(!l.empty()){
-            for(int i = 1; i < 1000; i++){
-                string h2;
-                bool verified;
-                if(hashes.size() <= counter + i){
-                    string hash = md5(input + to_string(counter + i));
-                    hashes.push_back(hash);
-                    h2 = hash;
-                }
-                else{
-                    h2 = hashes[counter + i];
-                }
-                for(auto c : l){
-                    if(verify_repeat(h2, 5, c)){
-                        keys.push_back(counter);
-                        cout << keys.size() << ":" << counter << endl;
-                        verified = true;
-                        break;
-                    }
-                }
-                if(verified){
-                    break;
-                }
-            }
-        }
-        counter++;
-    }
-    return 0;
-}
-
-*/
